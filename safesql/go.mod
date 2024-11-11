9ca6c73d86589ad00c9b166ddbdb70d2bff79b06
Prajjwal <pm@micobo.com>
2024-11-11 12:16:33 +0100

    upgrade: safesql upgrade

--- safesql/go.mod
+++ safesql/go.mod
@@ -1,3 +1,5 @@
 module github.com/darksidergod/def-prog-exercises/safesql
 
 go 1.23.1
+
+require github.com/darksidergod/def-prog-exercises v0.0.0-20241111104928-8c41eeeb300f
--- safesql/safesql.go
+++ safesql/safesql.go
@@ -1,6 +1,10 @@
 package safesql
 
-import "github.com/darksidergod/def-prog-exercises/safesql/internal/raw"
+import (
+	"database/sql"
+
+	"github.com/darksidergod/def-prog-exercises/safesql/internal/raw"
+)
 
 type compileTimeConstant string
 
@@ -12,6 +16,11 @@ func New(text compileTimeConstant) TrustedSQL {
 	return TrustedSQL{
 		string(text),
 	}
+}
+
+func Open(driverName, dataSourceName string) (*DB, error) {
+	d, err := sql.Open(driverName, dataSourceName)
+	return &DB{d}, err
 }
 
 func init() {
